dc00f72e28f0b813c98698773fc448fea5e88cf4
JellyJoe13 <johannes.p.urban@gmail.com>
2024-03-05 20:38:32 +0100

    wrote batch collation for ptree

--- ptgnn/loading/collate.py
+++ ptgnn/loading/collate.py
@@ -1,5 +1,7 @@
+import json
 from typing import Optional, Iterable, List
 
+from torch_geometric.data import Batch
 from torch_geometric.data.data import BaseData
 from torch_geometric.loader.dataloader import Collater
 
@@ -23,6 +25,7 @@ class UniversalCollater:
 
         # construct default collator
         self.default_collator = Collater(
+            dataset=None,
             follow_batch=follow_batch,
             exclude_keys=exclude_keys
         )
@@ -54,8 +57,57 @@ class UniversalCollater:
                 return self.default_collator(batch)
 
 
+def permutation_tree_batching(data_list: List[BaseData]):
+    # make container for transformed ptree strings
+    ptree_batch_list = []
+
+    # iterate over elements to be put into batch
+    for data in data_list:
+
+        # extract ptree string
+        ptree_list = data.ptree
+        # create dict from it
+        ptree_list = [json.loads(ptree_string) for ptree_string in ptree_list]
+
+        # elevate numbers in dict - will probably need to be recursive - check for int and if int then increase
+        def _increase_int(d, increase):
+            if increase == 0:
+                return d
+            if isinstance(d, dict):
+                for key, value in d.items():
+                    d[key] = _increase_int(value, increase)
+                return d
+            elif isinstance(d, list):
+                return [
+                    _increase_int(elem, increase)
+                    for elem in d
+                ]
+            else:
+                return d + increase
+        ptree_list = [
+            _increase_int(
+                ptree_dict,
+                increase=len(ptree_batch_list)
+            )
+            for ptree_dict in ptree_list
+        ]
+        # revert to string
+        ptree_list = [
+            json.dumps(ptree_dict)
+            for ptree_dict in ptree_list
+        ]
+        # put into storage
+        ptree_batch_list += ptree_list
+
+    return ptree_batch_list
+
+
 def permutation_tree_collation(
-        batch: List[BaseData]
+        data_list: List[BaseData]
 ):
-    # todo
-    pass
+    batch = Batch.from_data_list(
+        data_list=data_list,
+        exclude_keys=['ptree']
+    )
+    batch.ptree = permutation_tree_batching(data_list)
+    return batch
