21eb4d14075422e07598cfa2363708b30a05d779
JellyJoe13 <johannes.p.urban@gmail.com>
2024-03-05 11:52:02 +0100

    added binding affinity dataset and added function typing

--- ptgnn/dataset/__init__.py
+++ ptgnn/dataset/__init__.py
@@ -1,1 +1,2 @@
 from .rs_dataset import RSDataset
+from .bindingaffinity_dataset import BindingAffinityDataset
--- ptgnn/dataset/bindingaffinity_dataset.py
+++ ptgnn/dataset/bindingaffinity_dataset.py
@@ -3,8 +3,8 @@ import pickle
 import typing
 
 import torch
-import torch_geometric as pyg
 from multiprocess.pool import Pool
+from torch_geometric.data import InMemoryDataset
 from tqdm import tqdm
 
 from ptgnn.dataset.utils_chienn import download_url_to_path
@@ -12,15 +12,16 @@ from ptgnn.masking import MASKING_MAPPING
 from ptgnn.transform import PRE_TRANSFORM_MAPPING
 
 
-class RSDataset(pyg.data.InMemoryDataset):
+class BindingAffinityDataset(InMemoryDataset):
     """
     Heavily based and modified(optimized) from
-    https://github.com/gmum/ChiENN/blob/ee3185b39e8469a8caacf3d6d45a04c4a1cfff5b/experiments/graphgps/dataset/rs_dataset.py
+    https://github.com/gmum/ChiENN/blob/master/experiments/graphgps/dataset/binding_affinity_dataset.py
     """
     def __init__(
             self,
-            root: str = "src/rs",
+            root: str = "src/bindingaffinity",
             single_conformer: bool = True,
+            single_enantiomer: bool = False,
             mask_chiral_tags: bool = False,
             split: str = "train",
             graph_mode: str = "edge",
@@ -29,32 +30,15 @@ class RSDataset(pyg.data.InMemoryDataset):
             max_atoms: int = 100,
             max_attempts: int = 100,  # significantly decreased - 5000 is way too much!
     ):
-        """
-        Init of the RS dataset class. Starts the downloading and processing if the corresponding files are not present.
-        Uses multiprocess library to speed up computation by using all CPU cores.
-
-        :param root: Path to which the dataset should be saved
-        :param single_conformer: Filtering option removing duplicate entries using ID column of data
-        :param mask_chiral_tags: If true, removes part of features of data related to chiral tags. Affects node features
-            and edge attributes
-        :param split: Determines whether test, train or val dataset should be loaded
-        :param graph_mode: Determines whether an edge graph or vertex graph should be produced
-        :param transformation_mode: Specifies the mode of transformation. Currently available: ``default``=``chienn``,
-            ``permutationTree``.
-        :param transformation_parameters: Configurable options of the transformation.
-        :param max_atoms: Max number of atoms - required for creating embedding
-        :param max_attempts: Maximal attempts of creating a 3d version of the molecule. Warning: Do not set too high
-            as some molecules cannot be 'rendered' in reasonable time.
-        """
-        # link storage - where to fetch the data from
         self.link_storage = {
-            'train': 'https://figshare.com/ndownloader/files/30975694?private_link=e23be65a884ce7fc8543',
-            'val': 'https://figshare.com/ndownloader/files/30975703?private_link=e23be65a884ce7fc8543',
-            'test': 'https://figshare.com/ndownloader/files/30975679?private_link=e23be65a884ce7fc8543'
+            'train': 'https://figshare.com/ndownloader/files/30975697?private_link=e23be65a884ce7fc8543',
+            'val': 'https://figshare.com/ndownloader/files/30975706?private_link=e23be65a884ce7fc8543',
+            'test': 'https://figshare.com/ndownloader/files/30975682?private_link=e23be65a884ce7fc8543'
         }
 
         # set internal parameters
         self.single_conformer = single_conformer
+        self.single_enantiomer = single_enantiomer
         self.mask_chiral_tags = mask_chiral_tags
         self.split = split
         self.graph_mode = graph_mode
@@ -72,19 +56,21 @@ class RSDataset(pyg.data.InMemoryDataset):
             pre_transform=self.pre_transform,
             pre_filter=None
         )
-
         # part that actually loads the data into the class
+
         self.data, self.slices = torch.load(os.path.join(self.processed_dir, f"{split}.pt"))
 
     @property
     def raw_file_names(self) -> typing.Union[str, typing.List[str], typing.Tuple[str, ...]]:
-        return f'{self.split}.pickle'
+        return f"{self.split}.pickle"
 
     @property
     def processed_dir(self) -> str:
-        name = 'single_conformer' if self.single_conformer else 'all_conformers'
+        directory = 'single_conformer' if self.single_conformer else 'all_conformers'
+        if self.single_enantiomer:
+            directory = directory + "+single_enantiomer"
         graph_mode = self.graph_mode if self.graph_mode else ''
-        return os.path.join(self.root, name, graph_mode, 'processed')
+        return os.path.join(self.root, directory, graph_mode, 'processed')
 
     @property
     def processed_file_names(self) -> typing.Union[str, typing.List[str], typing.Tuple[str, ...]]:
@@ -102,6 +88,9 @@ class RSDataset(pyg.data.InMemoryDataset):
 
         if self.single_conformer:
             split_df = split_df.drop_duplicates(subset="ID")
+
+        if self.single_enantiomer:
+            split_df = split_df.group_by('SMILES_nostereo').sample(n=1, random_state=0)
 
         # iterate over dataframe (multiprocessing)
         def worker(entry):
@@ -142,7 +131,7 @@ class RSDataset(pyg.data.InMemoryDataset):
                 data = self.pre_transform(data, self.transformation_mode, self.transformation_parameters)
 
             # set label and append
-            data.y = torch.tensor(row['RS_label_binary']).long()
+            data.y = torch.tensor(row['top_score']).float()
 
             return smiles_nonstereo, data
 
