f7bfb3dfdc6426b93a4418162cb5397369a45dca
JellyJoe13 <johannes.p.urban@gmail.com>
2024-03-06 21:43:00 +0100

    added masking for vertex and edge graph from chienn (with more explanation and more centralized variables)

--- ptgnn/masking/edge_graph/__init__.py
+++ ptgnn/masking/edge_graph/__init__.py
@@ -1,2 +1,38 @@
-def edge_graph_masking():
-    pass
+import torch_geometric as pyg
+from ..vertex_graph import num_default_node_feature_last_chiral, num_default_edge_attr_last_chiral
+
+
+def edge_graph_masking(data: pyg.data.Data) -> pyg.data.Data:
+    """
+    Based on the rules from ``ptgnn.masking.vertex_graph.vertex_graph_masking(...)`` in combination with the edge graph
+    transformation in ``ptgnn.transfrom.edge_graph.to_edge_graph.to_edge_graph(...)`` this behavior is created.
+    Adapted/rewritten from:
+    https://github.com/gmum/ChiENN/blob/ee3185b39e8469a8caacf3d6d45a04c4a1cfff5b/experiments/graphgps/dataset/utils.py#L220
+    """
+    # get dimension shapes
+    edge_dim = 14
+    node_dim = 52
+
+    # get how many chiral positions need to be masked
+    n_node_chiral = num_default_node_feature_last_chiral
+    n_edge_chiral = num_default_edge_attr_last_chiral
+
+    # new node is an edge (in the edge graph)
+    # data for this consists of node | edge | node
+    # first part (node)
+    data.x[:, node_dim - n_node_chiral: node_dim] = 0.
+    # second part (edge)
+    data.x[:, node_dim + edge_dim - n_edge_chiral: node_dim + edge_dim] = 0.
+    # third part (node)
+    data.x[:, node_dim + edge_dim + node_dim - n_node_chiral: node_dim + edge_dim + node_dim] = 0.
+
+    # new edge is a node connection of two edges
+    # data for this consists of edge | node | edge
+    # first part (edge)
+    data.edge_attr[:, edge_dim - n_edge_chiral: edge_dim] = 0.
+    # second part (node)
+    data.edge_attr[:, edge_dim + node_dim - n_node_chiral: edge_dim + node_dim] = 0.
+    # third part (edge)
+    data.edge_attr[:, edge_dim + node_dim + edge_dim - n_edge_chiral: edge_dim + node_dim + edge_dim] = 0.
+
+    return data
--- ptgnn/masking/vertex_graph/__init__.py
+++ ptgnn/masking/vertex_graph/__init__.py
@@ -1,2 +1,16 @@
-def vertex_graph_masking():
-    pass
+import torch_geometric as pyg
+
+num_default_node_feature_last_chiral = 9
+num_default_edge_attr_last_chiral = 7
+
+
+def vertex_graph_masking(data: pyg.data.Data) -> pyg.data.Data:
+    """
+    According to
+    https://github.com/gmum/ChiENN/blob/ee3185b39e8469a8caacf3d6d45a04c4a1cfff5b/experiments/graphgps/dataset/utils.py#L209
+    is to remove last 9 elements from node features and last 7 from edge features. (Removing by zeroing)
+    """
+    data.x[:, -num_default_node_feature_last_chiral:] = 0.
+    data.edge_attr[:, -num_default_edge_attr_last_chiral] = 0.
+
+    return data
