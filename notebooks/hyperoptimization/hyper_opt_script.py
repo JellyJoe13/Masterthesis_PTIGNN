469a8566a5e4f27518333b531e39195bf58882ee
JellyJoe13 <johannes.p.urban@gmail.com>
2024-03-27 14:01:00 +0100

    executed ptree rs hyperopt (default)

--- notebooks/hyperoptimization/hyper_opt_script.py
+++ notebooks/hyperoptimization/hyper_opt_script.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 
     # make sure that output_dir exists
     if not os.path.exists(output_path):
-        os.mkdir(output_path)
+        os.mkdirs(output_path)
 
     # save general configs
     export_as(default_config, os.path.join(output_path, "general_config.yaml"), save_type='yaml')
