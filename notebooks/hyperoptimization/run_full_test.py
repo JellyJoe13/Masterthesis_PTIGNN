a59e826db7f6185b93b690f84aaf745b73d8cb60
JellyJoe13 <johannes.p.urban@gmail.com>
2024-03-28 12:46:47 +0100

    added full run script and instructions for rs/ba

---
+++ notebooks/hyperoptimization/run_full_test.py
@@ -0,0 +1,113 @@
+import argparse
+import os
+
+import pandas as pd
+
+from ptgnn.runtime_config.config import import_as, priority_merge_config, export_as
+from ptgnn.runtime_config.config_helpers import load_and_merge_default_configs
+from ptgnn.runtime_config.run_config import run_config
+
+
+def do_parsing():
+    parser = argparse.ArgumentParser(description="Process input config path.")
+    parser.add_argument("config_path", metavar="p", type=str, nargs=1, help="path to config file")
+
+    parser.add_argument(
+        "--verbose",
+        dest='verbose',
+        action='store_const',
+        const=True,
+        default=False,
+        help="Whether or not to print auxiliary information."
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # parse argument:
+    # - config_file_path
+    # - verbose
+    # - ...
+    args = do_parsing()
+
+    # load config file
+    config_path = args.config_path[0]
+    test_config = import_as(config_path)
+
+    # check if verbose flag set
+    verbose = args.verbose
+
+    # load default config
+    default_config = load_and_merge_default_configs(
+        test_config['config_files']
+    )
+
+    # load results
+    loading_dir = os.path.abspath(test_config['loading_dir'])
+    results = pd.read_csv(os.path.join(loading_dir, "results.csv"))
+    if verbose:
+        print(results)
+
+    n_best = test_config['n_best']
+
+    # fetch optimization_metric
+    optimization_metric = default_config['training']['optimization_metric']
+    optimization_metric_mode = default_config['training']['optimization_metric_mode']
+
+    # select n_best trials
+    metric_val_sorted_idx = results[f"val_{optimization_metric}"].argsort()
+
+    # in case metric is an accuracy value, then invert sort order
+    if optimization_metric_mode == 'max':
+        metric_val_sorted_idx = metric_val_sorted_idx[::-1]
+
+    if verbose:
+        print(metric_val_sorted_idx)
+
+    # define output directory
+    output_dir = os.path.join(loading_dir, "final")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # iterate over few selected indices and run experiment
+    for i, idx in enumerate(metric_val_sorted_idx[:n_best]):
+
+        # get trial identifier
+        trial_identifier = results.iloc[idx].logdir
+
+        # load config
+        local_config = priority_merge_config(
+            import_as(
+                os.path.join(os.path.abspath(test_config['loading_dir']), f"{trial_identifier}.yaml"),
+                loading_type='yaml'
+            ),
+            default_config
+        )
+
+        if verbose:
+            print(local_config)
+
+        # run config with test
+        full_trial_df = pd.DataFrame([1,2,3])
+        #todo: include test dataset and check
+        #full_trial_df = run_config(
+        #    local_config,
+        #    report=False,
+        #    verbose=verbose
+        #)
+
+        # define output name
+        output_name = f"rank{i+1}_{trial_identifier}_"
+
+        # save config
+        export_as(
+            local_config,
+            path=os.path.join(output_dir, output_name + "config.yaml"),
+            save_type='yaml'
+        )
+        # save results
+        full_trial_df.to_csv(
+            os.path.join(output_dir, output_name + "results.csv"),
+            index=None
+        )
