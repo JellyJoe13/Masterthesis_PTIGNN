29c1b054afb00de0e6bc547f516ea258f0b2c786
JellyJoe13 <johannes.p.urban@gmail.com>
2024-04-06 13:38:26 +0200

    current save

--- ptgnn/dataset/__init__.py
+++ ptgnn/dataset/__init__.py
@@ -2,9 +2,11 @@ from .rs_dataset import RSDataset
 from .bindingaffinity_dataset import BindingAffinityDataset
 from .csv_datasets import BaceDataset, Tox21Dataset
 from .ogb_datasets import OGBDataset
+from .ez_dataset import EZDataset
 
 DATASET_DICT = {
     "rs": RSDataset,
+    "ez": EZDataset,
     "ba": BindingAffinityDataset,
     "bace": BaceDataset,
     "tox21": Tox21Dataset,
