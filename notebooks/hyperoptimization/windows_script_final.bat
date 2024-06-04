e2596baa08b313d5d3200c487c12d887b58e8ef4
JellyJoe13 <johannes.p.urban@gmail.com>
2024-06-04 10:12:34 +0200

    current state of hyperopt template notebook and final script

--- notebooks/hyperoptimization/windows_script_final.bat
+++ notebooks/hyperoptimization/windows_script_final.bat
@@ -4,14 +4,14 @@ call conda activate Masterarbeit_PTGNN
 
 pause
 
-call python full_test_script.py "configs/final_test/bace/instructions_bace_ptree_edge_default.yaml" --verbose
+call python hyper_opt_script_v3.py "configs/benchmarking/tox21/benchmark_instructions_tox21_ptree_vertex_default_e_linred.yaml" --verbose -device cpu -cpu 5 -gpu 0
 
-call python full_test_script.py "configs/final_test/bace/instructions_bace_ptree_edge_selective_rwse.yaml" --verbose
-
-call python full_test_script.py "configs/final_test/bace/instructions_bace_ptree_vertex_default_rwse.yaml" --verbose
-
-call python full_test_script.py "configs/final_test/bace/instructions_bace_ptree_vertex_selective_rwse.yaml" --verbose
+call python full_test_script.py "configs/final_test/tox21/instructions_tox21_ptree_vertex_default_e_linred.yaml" --verbose -device cpu -cpu 5 -gpu 0
 
 pause
+
+call python hyper_opt_script_v3.py "configs/benchmarking/rs/benchmark_instructions_rs_ptree_vertex_default_e_linred.yaml" --verbose -device cpu -cpu 5 -gpu 0
+
+call python full_test_script.py "configs/final_test/rs/instructions_rs_ptree_vertex_default_e_linred.yaml" --verbose -device cpu -cpu 8 -gpu 0
 
 call conda deactivate
